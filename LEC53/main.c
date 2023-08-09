42d92e6f6f5a43eddda040ddb797885ce239080c
Pronoy <pronoyroy782@gmail.com>
2023-08-09 06:38:25 +0530

    LEC53 file is completed no due...

--- LEC53/main.c
+++ LEC53/main.c
@@ -3,8 +3,22 @@ int main(){
     //!NULL POINTER
     //* A null pointer is a pointer that points to NULL(nothing)
     int a = 34;
-    int* ptr = NULL;
-    // printf("THE VALUE OF ptr is %d",*ptr);
-    ptr = &a;
+    int* ptr = &a;
+
+
+    if (ptr!=NULL)
+    {
+    printf("THE address of a is %d\n",*ptr);
+    }
+
+
+    else
+    {
+        printf("The pointer is a null pointer and cannot be dereferenced\n");
+    }
+
+    
+    //?The address of a is 0
+    //!There's a huge difference b/w void pointer and null pointer
     return 0;
 }
