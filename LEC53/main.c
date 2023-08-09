8dfaf9154d81fe7b60f67489b376c17dfb9ff82f
Pronoy <pronoyroy782@gmail.com>
2023-08-09 06:30:38 +0530

    modification in main.c in files LEC53

--- LEC53/main.c
+++ LEC53/main.c
@@ -2,10 +2,9 @@
 int main(){
     //!NULL POINTER
     //* A null pointer is a pointer that points to NULL(nothing)
-    int a = 10;
-    int *ptr = NULL;
+    int a = 34;
+    int* ptr = NULL;
     // printf("THE VALUE OF ptr is %d",*ptr);
     ptr = &a;
-    // printf("THE VALUE OF ptr is %d",*ptr);
     return 0;
 }
